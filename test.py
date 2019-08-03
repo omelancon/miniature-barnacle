8b50367d92f7c71cfef7670378b0e911b358bebb
Olivier Melançon <ol.melancon@gmail.com>
2019-08-03 11:03:26 -0400

    testing this project idea with latest compyl version and features

--- test.py
+++ test.py
@@ -1,120 +1,78 @@
 #! -*- coding:utf-8 -*-
 #!/usr/bin/env python3
 
+from collections import defaultdict
 
-from compyl.lexer import Lexer
-import re
-
-lrules = [
-    (r"([A-Z][a-z]?)(\d+)?", "ATOM"),
-    (r"\(", "L_DEL"),
-    (r"(\))(\d+)?", "R_DEL"),
-
-]
-
-lexer = Lexer(rules=lrules)
-lexer.set_line_rule("\n")
-lexer.build()
+import compyl
 
 
-def use_only_known_dels(molecula):
-    replacements = [
-        ('\]', ')'),
-        ('\}', ')'),
-        ('\[', '('),
-        ('\{', '('),
-    ]
-    for old, new in replacements:
-        molecula = re.sub(old, new, molecula)
-
-    return molecula
+def arggetter(n):
+    return lambda *args: args[n]
 
 
-def find_matching_del(s):
-    pairs = []
-    stack = []
+def merge_dicts(d1, d2=None):
+    if d2 is None:
+        return d1
 
-    for i, token in enumerate(s):
-        if token == '(':
-            stack.append(i)
-        elif token == ')':
-            if len(stack) == 0:
-                raise IndexError("No matching closing parens at: " + str(i))
-            pairs.append((stack.pop(), i))
+    for atom in d2:
+        d1[atom] += d2[atom]
 
-    if len(stack) > 0:
-        raise IndexError("No matching opening parens at: " + str(stack.pop()))
-
-    return pairs
+    return d1
 
 
-def reduce_string(tokens):
-    reduced_string = ""
-    coef = re.search("\)(?P<num>\d+)?", tokens[-1].value).group("num")
-    while tokens:
-        token = tokens.pop(0)
-        if token.type == "ATOM":
-            atom = re.search("(?P<name>[A-Z][a-z]?)(?P<num>\d+)?", token.value).group("name")
-            coef1 = re.search("(?P<name>[A-Z][a-z]?)(?P<num>\d+)?", token.value).group("num")
-            if coef1 is None: coef1 = 1
-            reduced_string += str(atom) + str(int(coef1) * int(coef))
+def mul_dict(d, n):
+    for atom in d:
+        d[atom] *= int(n.value)
 
-    return reduced_string
+    return d
 
 
-def create_tokens(chain):
-    tokens = []
-    lexer.read(chain)
-    tk = lexer.lex()
-
-    while tk:
-        tokens.append(tk)
-        tk = lexer.lex()
-    return tokens
+def atomic_dict(atom):
+    return defaultdict(int, {atom.value: 1})
 
 
-def transform(molecula):
-    pairs = find_matching_del(molecula)
-
-    if pairs:
-        pair = pairs.pop(0)
-
-        sub = molecula[pair[0]:pair[1]+2]
-        red = reduce_string(create_tokens(sub))
-        new_molecula = molecula.replace(sub, red)
-
-        if '(' in new_molecula:
-            return transform(new_molecula)
-        else:
-            return new_molecula
-    else:
-        return molecula
+class Lexer(compyl.Lexer):
+    ATOM = r'[A-Z][a-z]?'
+    NUM = r'\d+'
+    L_PA = r'\('
+    R_PA = r'\)'
+    L_SQ = r'\['
+    R_SQ = r'\]'
+    L_BR = r'\{'
+    R_BR = r'\}'
 
 
-def create_dict(tokens):
-    adict = {}
-    for token in tokens:
-        if token.type == "ATOM":
-            atom = re.search("(?P<name>[A-Z][a-z]?)(?P<num>\d+)?", token.value).group("name")
-            coef = re.search("(?P<name>[A-Z][a-z]?)(?P<num>\d+)?", token.value).group("num")
-            if coef is None: coef = 1
-            adict[atom] = adict.get(atom, 0) + int(coef)
-    return adict
+class Parser(compyl.Parser, terminal='res'):
+    res = 'molecule', dict
+
+    molecule = ('component molecule?', merge_dicts)
+
+    component =\
+        ('L_PA molecule R_PA', arggetter(1)),\
+        ('L_SQ molecule R_SQ', arggetter(1)),\
+        ('L_BR molecule R_BR', arggetter(1)),\
+        ('component NUM', mul_dict),\
+        ('ATOM', atomic_dict)
+
+
+_lexer = Lexer()
+_parser = Parser()
+
+
+def count_atoms(formula):
+    _lexer.read(formula)
+    for tk in _lexer:
+        _parser.parse(tk)
+    return _parser.end()
+
 
 
 if __name__ == "__main__":
     # molecula = "Mg(OH)2"
     # molecula = "K4[ON(SO3)2]2"
-    # molecula = "COOH{C(CH3)2}3CH3"
-    molecula = "H2O"
+    molecula = "COOH{C(CH3)2}3CH3"
+    # molecula = "H2O"
 
-    print(molecula)
-    molecula = use_only_known_dels(molecula)
-
-    my_molecula = transform(molecula)
-    print(my_molecula)
-
-    my_tokens = create_tokens(my_molecula)
-    my_dict = create_dict(my_tokens)
+    my_dict = count_atoms(molecula)
 
     print(my_dict)
